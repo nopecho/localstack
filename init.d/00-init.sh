140dbfd54f3f338a386f193d1fc77cbfb9bb5eab
nopecho <ch.jooon@gmail.com>
2022-11-19 20:55:52 +0900

    init shell

--- init.d/00-init.sh
+++ init.d/00-init.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
+
+#create your SQS names
+SQS_NAME_1='sqs-1'
+SQS_NAME_2='sqs-2'
+#localstack default SQS url
+DEFAULT_SQS_URL_1='http://localhost:4566/000000000000/'$SQS_NAME_1
+DEFAULT_SQS_URL_2='http://localhost:4566/000000000000/'$SQS_NAME_2
+
+#create your SNS names
+SNS_NAME_1='sns-1'
+#localstack default SNS arn
+DEFAULT_SNS_ARN_1='arn:aws:sns:ap-northeast-2:000000000000:'$SNS_NAME_1
+
 echo "Init localstack..."
 
-awslocal sns create-topic --name my-sns
-awslocal sqs create-queue --queue-name sample-queue
+awslocal sqs create-queue --queue-name $SQS_NAME_1
+awslocal sqs create-queue --queue-name $SQS_NAME_2
+
+awslocal sns create-topic --name $SNS_NAME_1
+
+awslocal sns subscribe --topic-arn $DEFAULT_SNS_ARN_1 --protocol sqs --notification-endpoint $DEFAULT_SQS_URL_1
+awslocal sns subscribe --topic-arn $DEFAULT_SNS_ARN_1 --protocol sqs --notification-endpoint $DEFAULT_SQS_URL_2
+
+echo "SNS subscriptions list >>>>>"
+awslocal sns list-subscriptions
+
+echo "Created SQS list >>>>>>"
+awslocal sqs list-queues
 
 echo "Init localstack finished!"
