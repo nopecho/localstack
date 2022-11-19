2e3745b2231084d277180a5134cd7391bdf08a00
nopecho <ch.jooon@gmail.com>
2022-11-19 21:15:01 +0900

    sample

---
+++ localstack/src/test/java/com/nopecho/localstack/LocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nopecho.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
